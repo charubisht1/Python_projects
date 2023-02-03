3ed9b09088a91e934caed7bfea5fed7e10328ca1
charubisht1 <124318129+charubisht1@users.noreply.github.com>
2023-02-03 21:05:23 +0100

    Add files via upload
     Create an object-oriented program where it is possible to register and administrate hockey 
    teams in a youth hockey cup

---
+++ Main_program.py
@@ -0,0 +1,11 @@
+# Importing class create menu
+from Team import Team
+from datetime import date
+from itertools import count
+from create_menu import create_menu
+print("This is an object-oriented program which allows to register and administrate hockey teams in a youth hockey cup.")
+print("This program will keep track of all the teams that a user of the program creates and administers")
+
+# Running the method run()
+create_menu.run()
+
